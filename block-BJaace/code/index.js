f6a1e585a9edf3f45a4bd8507ade9f02927ef64e
pushpendra1992 <kumar161192@outlook.com>
2020-08-23 00:28:22 +0530

    complete

--- block-BJaace/code/index.js
+++ block-BJaace/code/index.js
@@ -11,22 +11,24 @@ console.log(colors[2]);
 // 6. Access the last color in the array and print it to the console using `console.log()`
 console.log(colors[colors.length - 1]);
 // 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
+
 colors[0] = "black";
 // 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
 colors[colors.length - 1] = "ultravoilet";
 // 9. Now, Find the length of the array named `colors`.
-console.log(colors.length):
-    // 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
-    let fourthColor = colors[3];
+console.log(colors.length);
+// 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
+let fourthColor = colors[3];
+console.log(fourthColor);
 // 11. Add another color to the end of the list.
 colors[colors.length] = "green";
 // 12. Add another color to the beginning of the list (use array method array.unshift('gray'))
-colors.unshift("white")
+colors.unshift("gray");
 // 13. Remove the first color from the starting of the array
 delete colors[0];
 // 14. Remove the last color from the end of list, and then print the length of the array to the console one more time.
 delete colors[colors.length - 1];
-console.log(colors);
+console.log(colors.length);
 // 15. Remove the second color from the end of list.
 delete colors[1];
 // 16. Write a for loop to iterate through every element in the array and print each element to the console.
